56c339d434587923d2a1d0ee9db66b829e6b9854
Thanh <58739801+thanhyx4@users.noreply.github.com>
2024-10-09 09:20:19 +0700

    Update run_01_move_to_processing.sh

--- QLAD-flow/scripts/run_01_move_to_processing.sh
+++ QLAD-flow/scripts/run_01_move_to_processing.sh
@@ -33,6 +33,7 @@
 INCOMING_DIR=$DATA_DIR/incoming
 PROCESSING_DIR=$DATA_DIR/processing
 SERVER=$1
+PCAP_TIME=5
 
 echo "[$(date)] : start move data for $SERVER"
 
@@ -40,16 +41,40 @@ if [ ! -d "$PROCESSING_DIR/$SERVER" ]; then
    mkdir -p $PROCESSING_DIR/$SERVER
 fi
 
-for f in $INCOMING_DIR/$SERVER/*pcap*.gz
-do
-  ! [ -f $f ] && continue
+file_count=$(find "$INCOMING_DIR/$SERVER" -type f -name "*.pcap.gz" | wc -l)
+if ((file_count * PCAP_TIME) % 10 == 0)
+then
+   for f in $INCOMING_DIR/$SERVER/*pcap*.gz
+   do
+     ! [ -f $f ] && continue
+   
+     #check if file is not opened, if not then move file
+     if ! [[ `lsof +D $INCOMING_DIR/$SERVER | grep $f` ]]
+     then
+        file=$(basename $f)
+        echo "[$(date)] : move $file $PROCESSING_DIR/$SERVER/$file"
+        mv $f $PROCESSING_DIR/$SERVER/$(echo $file | cut -d '_' -f1).pcap.gz
+     fi
+   done
+else
+   file_count = file_count -  ((file_count * PCAP_TIME) % 10)/PCAP_TIME
+   for f in $INCOMING_DIR/$SERVER/*pcap*.gz
+   do
+     ! [ -f $f ] && continue
+   
+     #check if file is not opened, if not then move file
+     if ! [[ `lsof +D $INCOMING_DIR/$SERVER | grep $f` ]]
+     then
+        file=$(basename $f)
+        echo "[$(date)] : move $file $PROCESSING_DIR/$SERVER/$file"
+        mv $f $PROCESSING_DIR/$SERVER/$(echo $file | cut -d '_' -f1).pcap.gz
+     fi
+     file_count = file_count - 1
+     if (file_count == 0)
+     then 
+        break
+      fi
+   done
+fi
 
-  #check if file is not opened, if not then move file
-  if ! [[ `lsof +D $INCOMING_DIR/$SERVER | grep $f` ]]
-  then
-     file=$(basename $f)
-     echo "[$(date)] : move $file $PROCESSING_DIR/$SERVER/$file"
-     mv $f $PROCESSING_DIR/$SERVER/$(echo $file | cut -d '_' -f1).pcap.gz
-  fi
-done
 echo "[$(date)] : done moving data for $SERVER"
